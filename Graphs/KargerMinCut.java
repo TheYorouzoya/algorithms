692841b2ff05ae1bb2c950c925ed15e389dfc329
TheYorouzoya <ratneshrastogimarch@gmail.com>
2023-05-13 01:43:56 +0530

    Added KargerMinCut implementation

--- Graphs/KargerMinCut.java
+++ Graphs/KargerMinCut.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 /*
  * Program to implement the Randomized Contraction algorithm for a given undirected graph
@@ -24,10 +25,12 @@ public class KargerMinCut {
 
     public static class AdjacencyList {
         private ArrayList<Integer> nodes;
+        private ArrayList<Integer> nodesReference;
         private ArrayList<LinkedList<Integer>> edges;
 
         public AdjacencyList() {
             this.nodes = new ArrayList<Integer>();
+            this.nodesReference = new ArrayList<Integer>();
             this.edges = new ArrayList<LinkedList<Integer>>();
         }
 
@@ -35,8 +38,28 @@ public class KargerMinCut {
             return this.nodes;
         }
 
+        public ArrayList<Integer> getNodesReference() {
+            return this.nodesReference;
+        }
+
         public ArrayList<LinkedList<Integer>> getEdges() {
             return this.edges;
+        }
+
+        public int lookupNodeReference(int node) {
+            if (nodesReference.get(node - 1) == node) {
+                return node;
+            } else {
+                return lookupNodeReference(nodesReference.get(node - 1));
+            }
+        }
+
+        public long graphSize() {
+            long size = 0;
+            for(int i = 0; i < edges.size(); i++) {
+                size += edges.get(i).size();
+            }
+            return size;
         }
 
         public void loadGraphFromFile(String filename) {
@@ -47,6 +70,7 @@ public class KargerMinCut {
                     String line = scanner.nextLine();
                     String[] lineNodes = line.split("\\s");
                     nodes.add(j, Integer.parseInt(lineNodes[0]));   // Add first element of line to nodes array
+                    nodesReference.add(j, Integer.parseInt(lineNodes[0]));
                     for (int i = 0; i < lineNodes.length; i++) {
                         // Add the line of nodes to the linked list
                         edges.get(j).add(Integer.parseInt(lineNodes[i]));
@@ -60,17 +84,59 @@ public class KargerMinCut {
         
     }
 
-    private static void contractRandomEdge(ArrayList<LinkedList<Integer>> adjList) {
-        // Select the node at random
-        // Select the edge node in the node's linked list at random
-        // Append this latter node's edge linked list to the end of the first node's linked list
-        // clear the latter node from the array
+    private static void contractRandomEdge(AdjacencyList adjList) {
+
+        // Select the initial node at random
+        int indexA = ThreadLocalRandom.current().nextInt(adjList.nodes.size());
+        int nodeA = adjList.nodes.get(indexA);
+        int nodeAIndex = nodeA - 1;
+
+        // Select the edge node in the previously selected node's linked list at random
+        int indexB = ThreadLocalRandom.current().nextInt(1, adjList.edges.get(nodeAIndex).size());
+        int nodeB = adjList.edges.get(nodeAIndex).get(indexB);
+        nodeB = adjList.lookupNodeReference(nodeB);
+        int nodeBIndex = nodeB - 1;
+
+        // Replace exchange node in the reference list
+        adjList.nodesReference.set(nodeA - 1, nodeB);
+
+        // Append the first node's linked list to this second edge node
+        adjList.edges.get(nodeBIndex).addAll(adjList.edges.get(nodeAIndex));
+
+        // Clean up all the self loops
+        for (int i = 1; i < adjList.edges.get(nodeBIndex).size(); i++) {
+            int current = adjList.lookupNodeReference(adjList.edges.get(nodeBIndex).get(i));
+            if (current == nodeA || current == nodeB) {
+                adjList.edges.get(nodeBIndex).remove(i);
+                i--;
+            }
+        }
+
+        // Alias the first node to point to the second node
+        adjList.edges.set(nodeAIndex, adjList.edges.get(nodeBIndex));
+
+        // Remove the first node from the list of nodes
+        adjList.nodes.remove(indexA);
+    }
+
+    private static int computeMinCut(AdjacencyList adjList) {
+        while (adjList.nodes.size() > 2) {
+            contractRandomEdge(adjList);
+        }
+        int finalNode = adjList.nodes.get(0);
+        return adjList.edges.get(finalNode - 1).size() - 1;
     }
 
     public static void main(String[] args) {
         String filename = "kargerTest.txt";
-        AdjacencyList adjList = new AdjacencyList();
-        adjList.loadGraphFromFile(filename);
-        System.out.println(adjList.edges.get(0));
+        int minCut = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < 200; i++) {
+            AdjacencyList adjList = new AdjacencyList();
+            adjList.loadGraphFromFile(filename);
+            minCut = Math.min(minCut, computeMinCut(adjList));
+        }
+        System.out.println("Minimum cut after 200 trials is: " + minCut);
+        
     }
 }
